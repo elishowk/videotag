94ba4d5a8bdc88374826c2b5f87bae357e78ab0b
Hanne Matthieu <matthieu.hanne@gmail.com>
2013-01-02 09:19:01 +0100

    add validate

--- page/model.js
+++ page/model.js
@@ -1,9 +1,23 @@
 /*global define*/
 
 define(['backbone'], function (Backbone) {
-  'use strict';
+    'use strict';
 
-  return Backbone.Tastypie.Model.extend({
-    'urlRoot': require.appConfig.poserApiUrl + 'page/',
-  });
+    return Backbone.Tastypie.Model.extend({
+        'urlRoot': require.appConfig.poserApiUrl + 'page/',
+        validation: {
+           video: [{
+                required: true,
+                msg: 'Veuillez entrer une url'
+            },
+            {
+                pattern: 'url',
+                msg: 'Veuillez entrer une url valide'
+            }],
+            title: {
+                required: true,
+                msg: 'Veuillez entrer un titre'
+            },
+        }
+    });
 });
--- user/model.js
+++ user/model.js
@@ -1,9 +1,49 @@
-/*global define*/
-
 define(['backbone'], function (Backbone) {
-  'use strict';
+    'use strict';
 
-  return Backbone.Tastypie.Model.extend({
-    'urlRoot': require.appConfig.poserApiUrl + 'user/'
-  });
+    return Backbone.Tastypie.Model.extend({
+        'urlRoot': require.appConfig.poserApiUrl + 'user/',
+        'validation': {
+            first_name: [{
+                required: false,
+                maxLength: 250,
+                msg: 'Un maximum de 250 caractères est autorisé'
+            },
+            {
+                pattern: /^[A-Za-z-\s]+$/,
+                msg: 'Seules les caractères alphanumeriques sont autorisés'
+            }],
+            last_name: [{
+                required: false,
+                maxLength: 250,
+                msg: 'Un maximum de 250 caractères est autorisé'
+            },
+            {
+                pattern: /^[A-Za-z-\s]+$/,
+                msg: 'Seules les caractères alphanumeriques sont autorisés'
+            }],
+            username: [{
+                required: true,
+                rangeLength: [8,250],
+                msg: 'Veuillez entrer un pseudo compris entre 8 et 250 caractères '
+            },
+            {
+                pattern: /^[A-Za-z0-9@.-]+$/,
+                msg: 'Votre pseudo contient un caractères non autorisé'
+            }],
+            email: [{
+                required: true,
+                msg: 'Veuillez entrer une adresse électronique'
+            },
+            {
+                pattern: 'email',
+                msg: 'Veuillez entrer une adresse électronique valide'
+            }],
+            someAttribute: function(value) {
+                if(value !== 'somevalue') {
+                    return 'Error message';
+                }
+            }
+        }
+    })
 });
