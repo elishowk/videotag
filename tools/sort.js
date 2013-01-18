8fbede23903ef91fab996a1af8d69f9c9fb8ccbe
elishowk <elishowk@nonutc.fr>
2013-01-18 18:41:12 +0100

    fisrt sort.js draft

---
+++ tools/sort.js
@@ -0,0 +1,21 @@
+define({
+    'global': function(messages) {
+        var users = _.reduce(messages, function(points, msg, index) {
+            if (points[msg.created_by] === undefined) {
+                points[msg.created_by] = [1, msg.created_at];
+            } else {
+                points[msg.created_by][0] += 1;
+                if (points[msg.created_by][1] < msg.created_at) {
+                    points[msg.created_by][1] = msg.created_at;
+                }
+            }
+            if (typeof msg.metadata.like == 'number') {
+                points[msg.created_by] += msg.metadata.like;
+            }
+            return points;
+        }, {});
+        return _.sortBy(users, function(scores) {
+            return scores[0] + scores[1];
+        });
+    }
+});
